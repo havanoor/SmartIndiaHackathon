13108dab48a501962027a6ab218a41aa3f7bcaf4
havanoor <havanoor@gmail.com>
2020-02-06 16:42:58 +0530

    Loan portal and email updated

--- SIH/settings.py
+++ SIH/settings.py
@@ -127,4 +127,15 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
+
+
 LOGIN_URL = '/login'
+
+
+
+EMAIL_BACKENED='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_PORT=587
+EMAIL_USE_TLS=True
+EMAIL_HOST_USER='hrishikesh2pv@gmail.com'
+EMAIL_HOST_PASSWORD='bstiztkywkfdadzj'  
